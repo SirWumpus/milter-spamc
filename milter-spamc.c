0bff3f695cb1c40236e7c85a5605d1b5588cd542
Anthony Howe <achowe@snert.com>
2023-07-12 08:30:49 -0400

    Remove unused variable.

--- milter-spamc.c
+++ milter-spamc.c
@@ -1361,8 +1361,6 @@ printInfo(void)
 int
 main(int argc, char **argv)
 {
-	int argi;
-
 	smfOptFile.initial = MILTER_CF;
 	smfOptPidFile.initial = PID_FILE;
 	smfOptRunUser.initial = RUN_AS_USER;
@@ -1372,7 +1370,7 @@ main(int argc, char **argv)
 
 	/* Parse command line options looking for a file= option. */
 	optionInit(optTable, smfOptTable, NULL);
-	argi = optionArrayL(argc, argv, optTable, smfOptTable, NULL);
+	(void) optionArrayL(argc, argv, optTable, smfOptTable, NULL);
 
 	/* Parse the option file followed by the command line options again. */
 	if (smfOptFile.string != NULL && *smfOptFile.string != '\0') {
